a6180faf3295d8d9d777cc7ec14898eb0a8a60d4
jinghf <3480551359@qq.com>
2021-04-09 17:15:14 +0800

    演示冲突修改

--- common_eureka/src/main/java/com/usian/EurekaApp.java
+++ common_eureka/src/main/java/com/usian/EurekaApp.java
@@ -8,7 +8,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class EurekaApp {
     public static void main(String[] args) {
-        System.out.println("我刚刚增加了内容。。。");
+        System.out.println("我刚刚增加了内容。。。我修改了");
+        System.out.println("我市新增的，002");
         SpringApplication.run(EurekaApp.class,args);
     }
 }
